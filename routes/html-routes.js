420f062d20239072672d3beb2d2d8fb5d44c46df
Brian K <karabinchak.brian@gmail.com>
2017-07-29 12:00:55 -0400

    added logout

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -142,4 +142,10 @@ module.exports = function(app, passport) {
         });
 
     });
+
+    app.get('/logout', function(req, res) {
+        req.logout(); 
+        res.redirect('/');
+    });
+    
 }
